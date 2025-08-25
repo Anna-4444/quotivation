bb2b238214b9b4cb63ab54a671630498619e7a1d
anna_b <nelson_anna9@hotmail.com>
2025-08-25 13:43:00 -0500

    Add fetch quotes api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Loader } from "react-feather";
 import "./App.css";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
+  
+  const fetchQuotes = async () => {
+    try {
+      setLoading(true)
+      const response = await fetch(quotesUrl);
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+      const results = await response.json();
+      setQuotes(results);
+
+    } catch (error) {
+      console.log("something went wrong", error.message)
+    }
+    setLoading(false)
+  };
+
+  useEffect(() => {
+    fetchQuotes()
+  }, [])
 
   return (
     <div className='App'>
       <Header />
-      <main></main>
+      <main>
+        {loading ? <Loader /> : JSON.stringify(quotes)}
+      </main>
       <Footer />
     </div>
   );
