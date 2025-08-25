c932a3281febe176ea27d20e8695c3621d667f9b
anna_b <nelson_anna9@hotmail.com>
2025-08-25 15:32:22 -0500

    Add filter by category option

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -1,12 +1,16 @@
 import React from "react";
 import QuoteCard from "./QuoteCard.js";
+import CategoryForm from "./CategoryForm.js";
 
-const Quotes = ({ quotes }) => {
+const Quotes = ({ quotes, categories, category, handleCategoryChange}) => {
     return (
         <section className="all-quotes">
             <div className="quotes wrapper">
+                <div className="category-header">
+                    <p>Browse through your collection of quotes</p>
+                    <CategoryForm categories={categories} category={category} handleCategoryChange={handleCategoryChange}/>
+                </div>
                 {quotes.map((quote) => (<QuoteCard key={quote.id} quote={quote}/>))}
-                {/* {JSON.stringify(quotes)} */}
             </div>
         </section>
     )
